1a2fc2a0545d69b529edc988c957c4c3a0c119d9
Nabil Basyir <nabilbasyir7345@gmail.com>
2026-08-27 10:44:28 +0800

    modul 8

--- backend/src/models/messageModel.js
+++ backend/src/models/messageModel.js
@@ -3,6 +3,9 @@ const db = require("../config/db");
 const createMessage = (data, callback) => {
   const query = "INSERT INTO contacts (name, email, subject, message) VALUES (?, ?, ?, ?)";
   db.query(query, [data.name, data.email, data.subject, data.message], (err, results) => {
+    if (err) {
+      console.error("Error saat insert ke tabel contacts:", err);
+    }
     callback(err, results);
   });
 };
@@ -10,6 +13,9 @@ const createMessage = (data, callback) => {
 const getAllMessages = (callback) => {
   const query = "SELECT * FROM contacts ORDER BY created_at DESC";
   db.query(query, (err, results) => {
+    if (err) {
+      console.error("Error saat mengambil data contacts:", err);
+    }
     callback(err, results);
   });
 };
